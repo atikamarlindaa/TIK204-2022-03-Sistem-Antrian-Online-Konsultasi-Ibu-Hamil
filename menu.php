287b6064507c2bebdd228af95909150d4f342653
aydiani <101510294+aydiani@users.noreply.github.com>
2022-04-17 22:42:31 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,57 @@
+                    <br />
+                    <ul class="sidebar-menu">
+                        <li class="active">
+                            <a href="index.php">
+                                <i class="glyphicon glyphicon-dashboard"></i> <span>Dashboard</span>
+                            </a>
+                        </li>
+                          
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="glyphicon glyphicon-link"></i>
+                                <span>Konsultasi Ibu Hamil</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="produk.php"><i class="fa fa-angle-double-right"></i> Tiket Antrian</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="glyphicon glyphicon-usd"></i> <span>Data Tiket Antrian</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Data Antrian</a></li>
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Konfirmasi Antrian</a></li>
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Status </a></li>
+                            </ul>
+                        </li>
+
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="glyphicon glyphicon-lock"></i> <span>Admin</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="admin.php"><i class="fa fa-angle-double-right"></i>Data Admin</a></li>
+                                <li><a href="input-admin.php"><i class="fa fa-angle-double-right"></i>Tambah Admin</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="glyphicon glyphicon-file"></i> <span>Laporan</span>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Laporan Data Antrian</a></li>
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Laporan Konfirmasi Antrian</a></li>
+                                <li><a href="404.php"><i class="fa fa-angle-double-right"></i> Laporan Konsultasi</a></li>
+                            </ul>
+                        </li>
+                        </ul>
+                       
+                        
+                        
